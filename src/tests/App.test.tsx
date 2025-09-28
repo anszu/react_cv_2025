01452c91c771b28fa48685940af2e74f5c9b8592
anszu <info@anikaszuppa.de>
2025-09-28 21:23:01 +0200

    (#4) Storybook Setup and first story

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-import { App } from './App';
+import { App } from 'src/App';
 
 describe('App', () => {
   it('renders headline', () => {
