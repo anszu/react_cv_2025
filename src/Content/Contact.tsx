7ead9be41d043c737c1126908a04e35400aa3d2a
anszu <info@anikaszuppa.de>
2025-10-30 11:46:21 +0100

    Fix: remove former overflow settings for contact for now, to do an overall rework later

--- src/Content/Contact.tsx
+++ src/Content/Contact.tsx
@@ -8,12 +8,10 @@ export const Contact = ({ data }: { data: ContactData }) => (
         <address className="not-italic">
             {data?.links.map((link, id) => (
                 <div key={`${link.label}-${id}`} className="flex">
-                    <div className="min-w-32 max-w-64 overflow-hidden mr-4 text-ellipsis">
-                        {link.label}:{' '}
-                    </div>
+                    <div className="min-w-32 mr-4">{link.label}: </div>
                     <a
                         href={link.url}
-                        className="underline hover:text-blue-500 hover:no-underline max-w-64 overflow-hidden text-ellipsis"
+                        className="underline hover:text-blue-500 hover:no-underline"
                     >
                         {link.url}
                     </a>
