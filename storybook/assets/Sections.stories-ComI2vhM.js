ab02b6743996f3ad5bf89ae240a0389d303f22a0
anszu <anszu@users.noreply.github.com>
2025-11-02 12:35:29 +0000

    deploy: b33a87ec14d0c1693f3747d4ef415170e41f8a55

--- storybook/assets/Sections.stories-ComI2vhM.js
+++
@@ -1,8 +0,0 @@
-import{j as r,a}from"./iframe-BgOUhMhG.js";import{S as t}from"./Sections-BHNyvB-T.js";import"./preload-helper-PPVm8Dsz.js";import"./Content-B2huI64Y.js";import"./Section-DQXiHvod.js";const u={title:"Sections",component:t},n={contentData:[],contentError:null,contentLoading:!1,sectionData:[],sectionLoading:!1,sectionError:null},e={},o={render:()=>r.jsx(a.Provider,{value:{...n,sectionLoading:!0},children:r.jsx(t,{})})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: () => <DataContext.Provider value={{
-    ...mockProviderValue,
-    sectionLoading: true
-  }}>
-            <Sections />
-        </DataContext.Provider>
-}`,...o.parameters?.docs?.source}}};const l=["Default","Loading"];export{e as Default,o as Loading,l as __namedExportsOrder,u as default};
