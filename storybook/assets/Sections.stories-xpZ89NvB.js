cff8619989cb8f07ee2d4f56627fab521fd65f54
anszu <anszu@users.noreply.github.com>
2025-11-12 08:14:25 +0000

    deploy: 6e045feabfa09d8ad4bacec2ed3cf955d50c58f9

---
+++ storybook/assets/Sections.stories-xpZ89NvB.js
@@ -0,0 +1,8 @@
+import{j as r,a}from"./iframe-BKrKqlpN.js";import{S as t}from"./Sections-Bkngs38Y.js";import"./preload-helper-PPVm8Dsz.js";import"./Content-B5_fpJx8.js";import"./List-DJY2KLqk.js";import"./Section-CHeUYvpE.js";const u={title:"Sections",component:t},n={contentData:[],contentError:null,contentLoading:!1,sectionData:[],sectionLoading:!1,sectionError:null},e={},o={render:()=>r.jsx(a.Provider,{value:{...n,sectionLoading:!0},children:r.jsx(t,{})})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: () => <DataContext.Provider value={{
+    ...mockProviderValue,
+    sectionLoading: true
+  }}>
+            <Sections />
+        </DataContext.Provider>
+}`,...o.parameters?.docs?.source}}};const l=["Default","Loading"];export{e as Default,o as Loading,l as __namedExportsOrder,u as default};
