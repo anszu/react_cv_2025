affd1a0f0a7c484388ad1202895c383071b7dfed
anszu <anszu@users.noreply.github.com>
2025-11-28 11:04:08 +0000

    deploy: 0b4aec60aa0beadea79b1fdf80d725a982cbc8a7

---
+++ storybook/assets/Section.stories-D1F_11zU.js
@@ -0,0 +1,14 @@
+import{S as n,j as o,a}from"./iframe-DT14dEsZ.js";import{S as r}from"./Section-BUtU9zos.js";import"./preload-helper-PPVm8Dsz.js";import"./Content-DAgskSE_.js";import"./List-BI3Mn9xO.js";import"./Match-DMUWXtjb.js";import"./InfoBox-CvE2yKaz.js";import"./LoadingSpinner-Ds4a1hLZ.js";const x={title:"Sections/Section",component:r},i={contentData:[],contentError:null,contentLoading:!1,sectionData:[],sectionLoading:!1,sectionError:null},e={args:{id:n.HEADING,title:"Header",icon:"📚"}},t={render:()=>o.jsx(a.Provider,{value:{...i,contentLoading:!0},children:o.jsx(r,{id:n.EXPERIENCE,title:"Experience",icon:"😎"})})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: SectionId.HEADING,
+    title: 'Header',
+    icon: '📚'
+  }
+}`,...e.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  render: () => <DataContext.Provider value={{
+    ...mockProviderValue,
+    contentLoading: true
+  }}>
+            <Section id={SectionId.EXPERIENCE} title="Experience" icon="😎" />
+        </DataContext.Provider>
+}`,...t.parameters?.docs?.source}}};const S=["Default","LoadingContent"];export{e as Default,t as LoadingContent,S as __namedExportsOrder,x as default};
---
+++ storybook/assets/Sections.stories-Cz0je2Q3.js
@@ -0,0 +1,8 @@
+import{j as r,a}from"./iframe-DT14dEsZ.js";import{S as t}from"./Sections-CXXRBqyS.js";import"./preload-helper-PPVm8Dsz.js";import"./Match-DMUWXtjb.js";import"./InfoBox-CvE2yKaz.js";import"./LoadingSpinner-Ds4a1hLZ.js";import"./Section-BUtU9zos.js";import"./Content-DAgskSE_.js";import"./List-BI3Mn9xO.js";const g={title:"Sections",component:t},n={contentData:[],contentError:null,contentLoading:!1,sectionData:[],sectionLoading:!1,sectionError:null},e={},o={render:()=>r.jsx(a.Provider,{value:{...n,sectionLoading:!0},children:r.jsx(t,{})})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: () => <DataContext.Provider value={{
+    ...mockProviderValue,
+    sectionLoading: true
+  }}>
+            <Sections />
+        </DataContext.Provider>
+}`,...o.parameters?.docs?.source}}};const f=["Default","Loading"];export{e as Default,o as Loading,f as __namedExportsOrder,g as default};
