6658b9a70715a56be7c579689b703c524e81ad56
anszu <anszu@users.noreply.github.com>
2026-01-13 12:53:41 +0000

    deploy: 38cfa898210d2dafcc54a89cb75d5ac11ce6347a

---
+++ storybook/assets/Sections.stories-DcXO13NT.js
@@ -0,0 +1,8 @@
+import{j as r,a}from"./iframe-M0XSngIW.js";import{S as t}from"./Sections-DBVzO5eW.js";import"./preload-helper-PPVm8Dsz.js";import"./Match-CD4lwBMA.js";import"./InfoBox-B6J4qu3W.js";import"./LoadingSpinner-ClgmGqyW.js";import"./Section-CutEoqgv.js";import"./Content-DGaGK9nr.js";import"./List-Dq3P29CY.js";const g={title:"Sections",component:t},n={contentData:[],contentError:null,contentLoading:!1,sectionData:[],sectionLoading:!1,sectionError:null},e={},o={render:()=>r.jsx(a.Provider,{value:{...n,sectionLoading:!0},children:r.jsx(t,{})})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: () => <DataContext.Provider value={{
+    ...mockProviderValue,
+    sectionLoading: true
+  }}>
+            <Sections />
+        </DataContext.Provider>
+}`,...o.parameters?.docs?.source}}};const f=["Default","Loading"];export{e as Default,o as Loading,f as __namedExportsOrder,g as default};
