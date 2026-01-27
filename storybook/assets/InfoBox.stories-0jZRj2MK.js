caac1845a02d0e07f49af14976b68fd01a2086a8
anszu <anszu@users.noreply.github.com>
2026-01-27 12:33:27 +0000

    deploy: e2a68d28ff7d647ceaf86dc7c805e68bdb2995c2

---
+++ storybook/assets/InfoBox.stories-0jZRj2MK.js
@@ -0,0 +1,7 @@
+import{j as t}from"./iframe-4AwdhyB0.js";import{I as s}from"./InfoBox-D4eN5Zzs.js";import"./preload-helper-PPVm8Dsz.js";import"./LoadingSpinner-BGVkvD73.js";const m={title:"Match/InfoBox",component:s},r={},e={render:()=>t.jsx(s,{loading:!0})},o={render:()=>t.jsx(s,{validationError:"Please provide text!"})},a={render:()=>t.jsx(s,{serverError:"An error occured on the server!"})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: () => <InfoBox loading />
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: () => <InfoBox validationError="Please provide text!" />
+}`,...o.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  render: () => <InfoBox serverError="An error occured on the server!" />
+}`,...a.parameters?.docs?.source}}};const p=["Default","Loading","ValidationError","ServerError"];export{r as Default,e as Loading,a as ServerError,o as ValidationError,p as __namedExportsOrder,m as default};
---
+++ storybook/assets/Sections.stories-DsdYSceJ.js
@@ -0,0 +1,8 @@
+import{j as r,a}from"./iframe-4AwdhyB0.js";import{S as t}from"./Sections-IWR2oe1N.js";import"./preload-helper-PPVm8Dsz.js";import"./Match-Dgs3e_HO.js";import"./InfoBox-D4eN5Zzs.js";import"./LoadingSpinner-BGVkvD73.js";import"./Section-BOtt-X6J.js";import"./Content-E4-Zi2qe.js";import"./List-toHrcX-5.js";const g={title:"Sections",component:t},n={contentData:[],contentError:null,contentLoading:!1,sectionData:[],sectionLoading:!1,sectionError:null},e={},o={render:()=>r.jsx(a.Provider,{value:{...n,sectionLoading:!0},children:r.jsx(t,{})})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: () => <DataContext.Provider value={{
+    ...mockProviderValue,
+    sectionLoading: true
+  }}>
+            <Sections />
+        </DataContext.Provider>
+}`,...o.parameters?.docs?.source}}};const f=["Default","Loading"];export{e as Default,o as Loading,f as __namedExportsOrder,g as default};
